3802697aed597b0d01b3e5f89ce3b27643b14ed0
Joseph Hart <josephrhart@gmail.com>
2022-09-29 14:32:38 -0500

    adding flurl example

---
+++ libraries/misc-libraries/MiscLibraries.Flurl/FlurlExamples.cs
@@ -0,0 +1,24 @@
+﻿using Flurl;
+using Flurl.Http;
+
+namespace MiscLibraries.Flurl;
+
+public class FlurlExamples
+{
+    readonly string _openWeatherApiKey;
+
+    public FlurlExamples(string openWeatherApiKey)
+    {
+        _openWeatherApiKey = openWeatherApiKey;
+    }
+
+    public async Task<string> GetWeatherData(double lat, double lon)
+    {
+        return await "https://api.openweathermap.org/data/2.5/weather"
+            .SetQueryParam("lat", lat)
+            .SetQueryParam("lon", lon)
+            .SetQueryParam("appid", _openWeatherApiKey)
+            // .SetQueryParams(new { lat, lon, appid = _openWeatherApiKey })
+            .GetStringAsync();
+    }
+}
